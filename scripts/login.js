e8e6f8e7183e5e88a1aaa78966ad6069322e565d
ShahSaminYasar <shahsaminyasarraihan@gmail.com>
2026-03-07 02:29:08 +0600

    Dynamically rendered all issue cards with issues fetched through API

---
+++ scripts/login.js
@@ -0,0 +1,12 @@
+loginForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const formData = new FormData(e.target);
+  const { username, password } = Object.fromEntries(formData.entries());
+
+  if (username === "admin" && password === "admin123") {
+    window.location.assign("/main.html");
+  } else {
+    alert("Invalid Credentials");
+  }
+});
