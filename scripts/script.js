f36e17667ffb958dbef9bb0797e7ce17e2aed061
ShahSaminYasar <shahsaminyasarraihan@gmail.com>
2026-03-07 02:58:23 +0600

    Implemented Tab switching and data filtering

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,7 @@
 const loginForm = document.getElementById("login-form");
 const issueCardsContainer = document.getElementById("issue-cards-container");
 const spinner = document.getElementById("spinner");
+const tabsContainer = document.getElementById("tabs-container");
+const issueCounter = document.getElementById("issue-counter");
+
+let issues;
